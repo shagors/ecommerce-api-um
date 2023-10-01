001fd947342b5ec94ddf0ebae555b878d67b0dbd
thtshajjad <shagors@users.noreply.github.com>
2023-10-01 14:16:38 +0600

    connect with mongodb and design user model and est it

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
+import dotenv from "dotenv";
 import express from "express";
+import dbConnect from "../config/dbConnect.js";
 
+dotenv.config();
+// db connection
+dbConnect();
 const app = express();
 
 export default app;
