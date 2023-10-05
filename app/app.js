85bc606d89171cf66a371f3748bffe83e112bbc1
thtshajjad <shagors@users.noreply.github.com>
2023-10-05 13:37:19 +0600

    add stripe webhooks and get payment all details and get the success data from backend and stripe server

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import Stripe from "stripe";
 import cors from "cors";
 import express from "express";
 import dbConnect from "../config/dbConnect.js";
@@ -25,6 +26,41 @@ app.use("/api/v1/brands", brandsRouter);
 app.use("/api/v1/colors", colorRouter);
 app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/orders", orderRouter);
+
+// stripe webhook
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret =
+  "whsec_675f6c301905ccbd158e1fba851bf9a894aa146a1e3c833d2ef84f7f9c4a6415";
+
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
+    let event;
+    console.log("event");
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        // Then define and call a function to handle the event payment_intent.succeeded
+        break;
+      // handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
+  }
+);
 
 // error middleware
 app.use(notFound);
